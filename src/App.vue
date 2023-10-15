1f8aaaa4a575eb494070162748af83dedf6e5792
CodeSandbox <support@codesandbox.io>
2023-10-15 01:14:41 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import Kanban from "./components/Kanban.vue";
+import { getData } from "./data";
+
+export default {
+  components: { Kanban },
+  data() {
+    const { columns, cards } = getData();
+    return { columns, cards };
+  },
+};
+</script>
+
+<template>
+  <Kanban :columns="columns" :cards="cards" />
+</template>
---
+++ src/components/Kanban.vue
@@ -0,0 +1,22 @@
+<script>
+import { Kanban } from "@dhx/trial-kanban";
+import "@dhx/trial-kanban/dist/kanban.css";
+
+export default {
+  props: ["cards", "columns"],
+
+  mounted() {
+    new Kanban(this.$refs.cont, {
+      cards: this.cards,
+      columns: this.columns,
+    });
+  },
+  unmounted() {
+    this.$refs.cont.innerHTML = "";
+  },
+};
+</script>
+
+<template>
+  <div ref="cont" style="width: 100%; height: 100%"></div>
+</template>
